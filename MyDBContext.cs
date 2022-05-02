f1b3e8519abe89fe5f9016738b8ad15d01dd4e37
Imam Gouse-github <102473685+ImamGouse@users.noreply.github.com>
2022-05-02 11:01:07 +0530

    Add files via upload

---
+++ MyDBContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Assignment.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("name=conn")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
